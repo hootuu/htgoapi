e3801e9bdbc9104966169d5905b7f252c7f369dd
Ganser <ganser.uu@gmail.com>
2023-10-25 16:51:22 +0800

    add gen sign content

--- htocol/http.go
+++ htocol/http.go
@@ -1,5 +1,7 @@
 package htocol
 
+import "sort"
+
 const (
 	HeaderPrefix    = "HOTU-"                    //Uniform prefix
 	HeaderNonce     = HeaderPrefix + "Nonce"     //Nonce
@@ -8,3 +10,30 @@ const (
 	HeaderVN        = HeaderPrefix + "Vn"
 	HeaderSP        = HeaderPrefix + "Sp"
 )
+
+func GenSignContent(dict map[string]string) string {
+	if len(dict) == 0 {
+		return ""
+	}
+	var keyValuePairs []struct {
+		Key   string
+		Value string
+	}
+	for k, v := range dict {
+		keyValuePairs = append(keyValuePairs, struct {
+			Key   string
+			Value string
+		}{k, v})
+	}
+
+	sort.Slice(keyValuePairs, func(i, j int) bool {
+		return keyValuePairs[i].Key < keyValuePairs[j].Key
+	})
+
+	content := ""
+	for _, pair := range keyValuePairs {
+		content += pair.Key + "=" + pair.Value + ";"
+	}
+
+	return content
+}
